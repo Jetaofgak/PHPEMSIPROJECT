6609ae99fd52a3723006fdc11f040df5e99fc49b
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2023-12-14 00:24:24 +0100

    database.php

--- musicwebsite/classes/database.php
+++ musicwebsite/classes/database.php
@@ -1,1 +1,40 @@
+<?php
+$servername = "localhost:3308";
+$username = "amine";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+mysqli_select_db( $conn,'testDb');
+
+// Create database
+$sql = "CREATE DATABASE testDb";
+if (mysqli_query($conn, $sql)) {
+echo "Database created successfully";
+} else {
+echo "Error creating database: " . mysqli_error($conn);
+}
+
+$query = "
+CREATE TABLE testDb.Clients (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50) UNIQUE,
+password VARCHAR(80),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+";
+if (mysqli_query($conn, $query)) {
+echo "Table Clients created successfully";
+} else {
+echo "Error creating table: " . mysqli_error($conn);
+}
+
+?>
 
