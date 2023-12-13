f8e136152776af47ec1e8061e37d84b617761f6e
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2023-12-14 00:31:29 +0100

    Update read.php

--- musicwebsite/classes/read.php
+++ musicwebsite/classes/read.php
@@ -1,3 +1,29 @@
+<?php
+include('city.php');
+//include connection file
+include('connection.php');
+   
+
+//create in instance of class Connection
+$connection = new Connection();
+
+
+//call the selectDatabase method
+$connection->selectDatabase('crudPoo6'); 
+
+ //include the client file
+ include('client.php');
+
+ 
+  //call the static selectAllClients method and store the result of the method in $clients
+  $clients = Client::selectAllClients('Clients',$connection->conn);
+
+  if(isset($_POST['submit'])){
+    $clients = Client::selectClientByCityId('Clients',$connection->conn,$_POST['cities']);
+
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +42,26 @@
 
     <br>
     <br>
+    <form method="post">
+
+    <div class="input-group">
+  <span class="input-group-btn">
+   
+    <button class="btn btn-success" type="submit" name="submit">Search</button>
+   
+  </span>
+  <select name='cities' class="form-select">
+                <option selected>Select a city</option>
+                <?php
+                        
+                        $cities=City::selectAllcities('Cities',$connection->conn);
+                        foreach($cities as $city){
+                                echo "<option value='$city[id]' >$city[name]</option>";
+
+                        }
+                    ?>
+  </select>
+  </form>
     <table class="table">
        <thead>
         <tr>
@@ -23,30 +69,34 @@
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
+            <th>City Name</th>
             <th>Action</th>
         </tr>
         </thead>
         <tbody>
 
         <?php
-        include("database.php");
-        $sql = "SELECT id, firstname, lastname,email FROM testDb.clients";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
+
+       
+       
+       
+     
+        foreach($clients as $row) {
+            
+            $city = City::selectCityById('Cities',$connection->conn,$row['idCity']);
            echo " <tr>
             <td>$row[id]</td>
             <td>$row[firstname]</td>
             <td>$row[lastname]</td>
             <td>$row[email]</td>
+            <td>$city[name]</td>
             <td>
             <a class ='btn btn-success btn-sm' href='update.php?id=$row[id]'>edit</a>
             <a class ='btn btn-danger btn-sm' href='delete.php?id=$row[id]'>delete</a>
             </td>
         </tr>";
         }
-        }
+        
         ?>
         </tbody>
         
