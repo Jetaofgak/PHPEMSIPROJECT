04e9aa3c235b2146bd802ad97cfa72a461f39e76
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2023-12-14 01:05:41 +0100

    Update database.php
    FULL QUERIES

--- musicwebsite/classes/database.php
+++ musicwebsite/classes/database.php
@@ -13,7 +13,8 @@ CREATE TABLE Users (
     Users_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Users_FullName VARCHAR(100) NOT NULL,
     Users_Email VARCHAR(255) NOT NULL,
-    Users_Password VARCHAR(255) NOT NULL   
+    Users_Password VARCHAR(255) NOT NULL
+    
     )
 ";
 $query = "
@@ -21,11 +22,28 @@ CREATE TABLE Song (
 Song_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Song_Name VARCHAR(255) NOT NULL,
 Song_Lenght_Second INT NOT NULL,
-Song_Artist VARCHAR(255) NOT NULL,
-Song_Band VARCHAR(255)
+Song_Artist VARCHAR(255) NOT NULL
+
 )
 ";
 
+$query1 = "
+CREATE TABLE PLAYLIST (
+Users_id INT,
+playlist_id INT AUTO_INCREMENT PRIMARY KEY,
+playlist_name VARCHAR(255) NOT NULL,
+FOREIGN KEY (Users_id) REFERENCES users(Users_id)
+)
+";
+$query2 = "
+CREATE TABLE PLAYLIST_SONGS (
+playlist_id INT,
+    song_id INT,
+    PRIMARY KEY (playlist_id, song_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id)
+)
+";
 //call the selectDatabase method to select the chap4Db
 $connection->selectDatabase('Users');
 
