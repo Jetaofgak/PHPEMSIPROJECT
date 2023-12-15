24a9e853e52511b50847c351594d43341791e5f2
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2023-12-15 02:29:28 +0100

    Update register.php

--- musicwebsite/register.php
+++ musicwebsite/register.php
@@ -4,9 +4,12 @@ include_once 'classes/User.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Process form data
     $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password = password_hash($Password, PASSWORD_DEFAULT)
     
     // Create a new user
-    $user = new User(uniqid(), $username);
+    $user = new User($username,$email,$password);
     
     // Redirect to the homepage or perform additional actions
     header('Location: index.php');
